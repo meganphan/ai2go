8b400cac0fbe91eb7cb711d74361a82c2f1b1c43
MeganPhan <kimhoang7994@gmail.com>
2019-06-12 15:40:29 -0500

    change

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 from flask import Flask, render_template, request
-from flaskext.mail import Mail, Message
 
-mail=Mail(app)
-app.config.update(
-	DEBUG=True,
-	#EMAIL SETTINGS
-	MAIL_SERVER='smtp.gmail.com',
-	MAIL_PORT=465,
-	MAIL_USE_SSL=True,
-	MAIL_USERNAME = 'gansteamtcu@gmail.com',
-	MAIL_PASSWORD = 'GansInSecurity'
-	)
-# import sendgrid
-# from sendgrid import SendGridAPIClient
-# from sendgrid.helpers.mail import *
-
-# sg = sendgrid.SendGridAPIClient('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')
-# message = Mail(
-#     from_email='kimhoang7994@gmail.com',
-#     to_emails='kimhoang7994@gmail.com',
-#     subject='Sending with Twilio SendGrid is Fun',
-#     html_content='<strong>and easy to do anywhere, even with Python</strong>')
 app = Flask(__name__)
 
 @app.route("/")
@@ -38,14 +17,6 @@ def authors():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
-
-@app.route('/form', methods=['POST'])
-def form():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
-    mail = Mail()
-    return render_template("contact.html")  
 
 if __name__ == "__main__":
     app.run(debug=True)
